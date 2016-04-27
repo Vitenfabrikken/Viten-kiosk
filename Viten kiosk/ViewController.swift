3f2babe856e6f02362ae2b246fa5b96445d49bc2
Asbjørn Rørvik <asbjornrorvik@hotmail.com>
2016-04-27 09:36:32 +0200

    Initial Commit

---
+++ Viten kiosk/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Viten kiosk
+//
+//  Created by Asbjørn Rørvik on 27.04.2016.
+//  Copyright © 2016 Vitenfabrikken. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
